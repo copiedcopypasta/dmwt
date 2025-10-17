6d923fdc8dfc8c3a8cceb7fac3c2f4a99e890549
2hoch1 <the2hoch1@gmail.com>
2025-10-17 18:29:56 +0200

    feat: testing components window

---
+++ src/app/components/index.ts
@@ -0,0 +1,34 @@
+/**
+ * Exports all components from specified subfolders static.
+ */
+
+/* Features Components*/
+
+/* Feedback Components */
+export { default as Alert } from './feedback/Alert';
+export { default as Badge } from './feedback/Badge';
+export { default as ProgressBar } from './feedback/Progressbar';
+export { default as Toast } from './feedback/Toast';
+
+/* Layout Components */
+export { default as Footer } from './layout/Footer';
+export { default as Header } from './layout/Header';
+export { default as Navbar } from './layout/Navbar';
+export { default as Sidebar } from './layout/Sidebar';
+
+/* UI-Content Components */
+export { default as Card } from './ui/content/Card';
+export { default as List } from './ui/content/List';
+export { default as Textarea } from './ui/content/Textarea';
+export { default as Tooltip } from './ui/content/Tooltip';
+
+/* UI-Input Components */
+export { default as Button } from './ui/input/Button';
+export { default as Inputfield } from './ui/input/Inputfield';
+export { default as Slider } from './ui/input/Slider';
+export { default as Switch } from './ui/input/Switch';
+/* UI-Media Components */
+export { default as Avatar } from './ui/media/Avatar';
+export { default as Carousel } from './ui/media/Carousel';
+export { default as Gallery } from './ui/media/Gallery';
+export { default as Rating } from './ui/media/Rating';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,130 @@
+import * as Components from './components/index';
+
 export default function Home() {
-  return <main>{/* Leere Seite */}</main>;
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className="pb-8 text-4xl font-bold">Component Tests:</h1>
+
+        <h2 className="sticky top-0 z-10 -translate-x-[200px] transform pb-4 text-2xl font-semibold">
+          Feedback Components:
+        </h2>
+        <section>
+          <section>
+            <p>Alert:</p>
+            <Components.Alert />
+          </section>
+
+          <section>
+            <p>Badge:</p>
+            <Components.Badge />
+          </section>
+
+          <section>
+            <p>Progressbar:</p>
+            <Components.ProgressBar value={60} />
+          </section>
+
+          <section>
+            <p>Toast:</p>
+            <Components.Toast />
+          </section>
+        </section>
+
+        <h2 className="sticky top-0 z-10 -translate-x-[200px] transform pb-4 text-2xl font-semibold">
+          Layout Components:
+        </h2>
+        <section>
+          <section>
+            <p>Footer:</p>
+            <Components.Footer />
+          </section>
+
+          <section>
+            <p>Header:</p>
+            <Components.Header />
+          </section>
+
+          <section>
+            <p>Navbar:</p>
+            <Components.Navbar />
+          </section>
+
+          <section>
+            <p>Sidebar:</p>
+            <Components.Sidebar />
+          </section>
+        </section>
+
+        <h2 className="sticky top-0 z-10 -translate-x-[200px] transform pb-4 text-2xl font-semibold">
+          UI-Content Components:
+        </h2>
+        <section>
+          <section>
+            <p>Card:</p>
+            <Components.Card />
+          </section>
+
+          <section>
+            <p>List:</p>
+            <Components.List />
+          </section>
+
+          <section>
+            <p>Textarea:</p>
+            <Components.Textarea />
+          </section>
+
+          <section>
+            <p>Tooltip:</p>
+            <Components.Tooltip />
+          </section>
+        </section>
+
+        <h2 className="sticky top-0 z-10 -translate-x-[200px] transform pb-4 text-2xl font-semibold">
+          UI-Input Components:
+        </h2>
+        <section>
+          <section>
+            <p>Button:</p>
+            <Components.Button />
+          </section>
+          <section>
+            <p>Inputfield:</p>
+            <Components.Inputfield />
+          </section>
+          <section>
+            <p>Slider:</p>
+            <Components.Slider />
+          </section>
+          <section>
+            <p>Switch:</p>
+            <Components.Switch />
+          </section>
+        </section>
+
+        <h2 className="sticky top-0 z-10 -translate-x-[200px] transform pb-4 text-2xl font-semibold">
+          UI-Media Components:
+        </h2>
+        <section>
+          <section>
+            <p>Avatar:</p>
+            <Components.Avatar />
+          </section>
+          <section>
+            <p>Carousel:</p>
+            <Components.Carousel />
+          </section>
+          <section>
+            <p>Gallery:</p>
+            <Components.Gallery />
+          </section>
+          <section>
+            <p>Rating:</p>
+            <Components.Rating />
+          </section>
+        </section>
+      </main>
+    </>
+  );
 }

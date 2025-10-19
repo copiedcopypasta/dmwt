0b82c6ca8b44d1802e1d60bca98e7d5960990dae
2hoch1 <the2hoch1@gmail.com>
2025-10-19 15:50:10 +0200

    chore(page): added sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import * as Components from './components/index';
 export default function Home() {
   return (
     <>
+      <Components.Sidebar />
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <h1 className="pb-8 text-4xl font-bold">Component Tests:</h1>
 

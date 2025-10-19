911c305c8e5e9497192066ffafeff5a3f3bf4363
pallepalinski <schweizer.starzach@gmx.de>
2025-10-19 15:51:51 +0200

    feat(list): added list

--- src/app/components/ui/content/List.tsx
+++ src/app/components/ui/content/List.tsx
@@ -1,10 +1,24 @@
-// Todo: Remove this comment, if you add props to the Interface
+// list component for displaying a list of items
 
 import React, { JSX } from 'react';
 
 /* eslint-disable-next-line @typescript-eslint/no-empty-object-type */
-interface ListProps {}
+interface ListProps<T> {
+  items?: T[];
+  renderItem?: (item: T, index: number) => JSX.Element;
+}
 
-export default function List({}: ListProps): JSX.Element {
-  return <></>;
+export default function List<T>({
+  items = [],
+  renderItem,
+}: ListProps<T>): JSX.Element {
+  return (
+    <ul className="divide-y divide-gray-200">
+      {items.map((item, index) => (
+        <li key={index} className="p-2">
+          {renderItem?.(item, index)}
+        </li>
+      ))}
+    </ul>
+  );
 }

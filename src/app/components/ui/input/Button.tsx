e7c62106db6ed38cf26088d70b1b89140b886c77
dev2xJ <jensjung05@gmail.com>
2025-10-19 13:47:56 +0200

    fix(Button): misspelling

--- src/app/components/ui/input/Button.tsx
+++ src/app/components/ui/input/Button.tsx
@@ -3,11 +3,11 @@ import React, { JSX } from 'react';
 /* eslint-disable-next-line @typescript-eslint/no-empty-object-type */
 interface ButtonProps {
   color?: "red" | "green" | "blue";
-  descrition: string;
+  description: string;
   link: string;
 }
 
-export default function Button({ color, descrition, link}: ButtonProps): JSX.Element {
+export default function Button({ color, description, link}: ButtonProps): JSX.Element {
   const colorClasses: Record<string, string> = {
     red: "bg-red-600 hover:bg-red-700 text-white",
     green: "bg-green-600 hover:bg-green-700 text-white",
@@ -19,7 +19,7 @@ export default function Button({ color, descrition, link}: ButtonProps): JSX.Ele
       href={link}
       className={'inline-block px-6 py-3 rounded-xl ${colorClasses[color]} shadow-sm'}
     >
-      {descrition}
+      {description}
     </a>
   );
 }

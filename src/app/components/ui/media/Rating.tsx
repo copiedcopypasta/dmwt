b0b58bb9a048e87a3856b0ffe74d0601348e832f
dev2xJ <jensjung05@gmail.com>
2025-10-19 12:51:11 +0200

    feat(Rating): add first Version
    Todo: Add defaultIcon

--- src/app/components/ui/media/Rating.tsx
+++ src/app/components/ui/media/Rating.tsx
@@ -1,10 +1,28 @@
-// Todo: Remove this comment, if you add props to the Interface
+// Todo: Add defaultIcon
 
 import React, { JSX } from 'react';
 
 /* eslint-disable-next-line @typescript-eslint/no-empty-object-type */
-interface RatingProps {}
+interface RatingProps {
+  value: number;
+  max: number;
+  icon?: React.ReactNode;
+}
 
-export default function Rating({}: RatingProps): JSX.Element {
-  return <></>;
+export default function Rating({ value, max, icon}: RatingProps): JSX.Element {
+  const defaultIcon = (
+    <svg></svg>
+  );
+
+  const RatingIcon = icon || defaultIcon;
+
+  return (
+    <div className="flex items-center">
+      {Array.from({ length: max}, (_, i) =>(
+        <span key={i} className={'flex-shrink-0 ${i < value ? "" : "text-gray-300"}'}>
+          {RatingIcon}
+        </span>
+      ))}
+    </div>
+  );
 }

71c5b949469d24f7a0ec202b0e003de40838ea3e
dev2xJ <jensjung05@gmail.com>
2025-10-19 11:23:47 +0200

    chore(Footer):fix indentation

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -14,34 +14,34 @@ interface FooterProps {
 
 export default function Footer({ links, copyright, barrierefreiheit}: FooterProps): JSX.Element {
   return (
-  <footer className="py-6 mt-8">
-    <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4">
+    <footer className="py-6 mt-8">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4">
 
-      {/*Links*/}
-      <nav className="flex flex-wrap justify-center gap-4">
-        {links.map((link) =>(
-          <a
-          key={link.href}
-          href={link.href}
-          className="hover:text-white"
-          >
+        {/*Links*/}
+        <nav className="flex flex-wrap justify-center gap-4">
+          {links.map((link) =>(
+            <a
+              key={link.href}
+              href={link.href}
+              className="hover:text-white"
+            >
             {link.label}
           </a>
         ))}
-      </nav>
+        </nav>
 
-      {/*Barrierefreiheit*/}
-      <div className="text-center">
-        <a href="#" className="hover:text-white">
-          {barrierefreiheit}
-        </a>
-      </div>
+        {/*Barrierefreiheit*/}
+        <div className="text-center">
+          <a href="#" className="hover:text-white">
+            {barrierefreiheit}
+          </a>
+        </div>
 
-      {/*Copyright*/}
-      <div className="text-center md:text-right">
-        {copyright}
+        {/*Copyright*/}
+        <div className="text-center md:text-right">
+          {copyright}
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   );
 }

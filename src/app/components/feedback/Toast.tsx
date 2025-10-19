118521f45ebea098df117585f80c9a365e12f6eb
dev2xJ <jensjung05@gmail.com>
2025-10-19 12:22:13 +0200

    feat(Toast): add first Version

--- src/app/components/feedback/Toast.tsx
+++ src/app/components/feedback/Toast.tsx
@@ -1,10 +1,23 @@
-// Todo: Remove this comment, if you add props to the Interface
-
 import React, { JSX } from 'react';
 
 /* eslint-disable-next-line @typescript-eslint/no-empty-object-type */
-interface ToastProps {}
+interface ToastProps {
+  icon?: React.ReactNode;
+  text: string;
+}
 
-export default function Toast({}: ToastProps): JSX.Element {
-  return <></>;
+export default function Toast({ icon, text}: ToastProps): JSX.Element {
+  return (
+    <div className="flex itmes- center gap-3 px-4 rounded-2x1 shadow-sm">
+      {/*Icon*/}
+      {icon && (
+        <div className="">
+          {icon}
+        </div>
+      )}
+
+      {/*Text*/}
+      <p className="">{text}</p>
+    </div>
+  );
 }

db1bc1f9036e128e25c753f97ec8613e10ade0c0
2hoch1 <the2hoch1@gmail.com>
2025-10-22 11:57:27 +0200

    chore: rename alert to banner

--- src/app/components/feedback/Banner.tsx
+++ src/app/components/feedback/Banner.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-type AlertType = 'success' | 'error' | 'warning' | 'info';
+type BannerType = 'success' | 'error' | 'warning' | 'info';
 
-interface AlertProps {
-  type?: AlertType;
+interface BannerProps {
+  type?: BannerType;
   message: string | undefined;
 }
 
-const alertStyles: Record<AlertType, string> = {
+const bannerStyles: Record<BannerType, string> = {
   success: 'bg-green-100 border-green-400 text-green-700',
   error: 'bg-red-100 border-red-400 text-red-700',
   warning: 'bg-yellow-100 border-yellow-400 text-yellow-700',
   info: 'bg-blue-100 border-blue-400 text-blue-700',
 };
 
-export default function Alert({
+export default function Banner({
   type,
   message,
-}: AlertProps): React.ReactElement {
+}: BannerProps): React.ReactElement {
   return (
-    <div className={`rounded p-3 ${alertStyles[type || 'info']} border`}>
+    <div className={`rounded p-3 ${bannerStyles[type || 'info']} border`}>
       {message}
     </div>
   );

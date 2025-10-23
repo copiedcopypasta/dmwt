c68a597c0b18ddc8fc9f958e6708849c062d6a35
2hoch1 <the2hoch1@gmail.com>
2025-10-23 15:04:22 +0200

    chore(prisma): update setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -22,8 +22,9 @@ export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: env('PRISMA_DATABASE_URL'),
   },
 });

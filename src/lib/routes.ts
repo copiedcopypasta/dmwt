07699fec77ae4636e1ad5be7f028639505fdf5d5
2hoch1 <mail@2hoch1.dev>
2026-01-03 02:10:53 +0100

    refactor: remove unused libs

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,8 +1,3 @@
-/**
- * DO NOT TOUCH THIS FILE!
- * This file is auto-generated. Any changes made here will be overwritten.
- */
-
 import path from 'path';
 
 const routes = [
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,8 +1,3 @@
-/**
- * DO NOT TOUCH THIS FILE!
- * This file is auto-generated. Any changes made here will be overwritten.
- */
-
 const theme = {
   backgroundColor: '#f0f0f0',
   themeColor: '#3498db',

cd673b1761f90f4ee69b3e894f5a9f9c86cf14d6
Henning Raudzis <henning.raudzis@student.reutlingen-university.de>
2026-01-16 13:10:52 +0100

    feat: dark/lightmode spinner

--- src/components/ui/base/spinner.tsx
+++ src/components/ui/base/spinner.tsx
@@ -1,12 +1,32 @@
+'use client';
+
+import { useTheme } from 'next-themes';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export function Spinner() {
+  const { theme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+  const spinnerSrc =
+    currentTheme === 'dark' ? '/spinner-dark.webp' : '/spinner-light.webp';
+
   return (
     <Image
-      src='/spinner.webp'
+      src={spinnerSrc}
       alt='Spinner loading animation'
       width={240}
       height={240}
+      preload={true}
     />
   );
 }
